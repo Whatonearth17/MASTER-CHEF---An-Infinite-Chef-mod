aa881c61f8e38f84712e6f2568f7eeb26979d50b
Whatonearth17 <Emadmichael13@gmail.com>
2025-02-04 18:23:05 -0500

    Update master_chef.js

--- master_chef.js
+++ master_chef.js
@@ -6,8 +6,8 @@
 //flowers
 
 addIngredient("sunflower",{
-    colour:"#c7721e",
     type:"decor",
+    colour:"#c7721e",
     shape:"flower",
     scale:0.5,
     keywords:"sun,flower,petal,",
@@ -16,8 +16,8 @@ addIngredient("sunflower",{
   });
 
 addIngredient("rose",{
-    colour:"#bc2ac9",
     type:"decor",
+    colour:"#bc2ac9",
     shape:"flower_rose",
     scale:0.5,
     keywords:"rose,flower,petal,",
@@ -26,10 +26,96 @@ addIngredient("rose",{
     });
 
 addIngredient("rose_petal",{
-    colour:"#bc2ac9",
     type:"decor",
+    colour:"#bc2ac9",
     shape:"bean",
     scale:0.5,
     dishName:"rose_petal",
     keywords:"rose,flower,petal,"
+});
+
+//dyes
+
+addIngredient("orange_dye",{
+    type:"red_dye",
+    color:"#FFBB00",
+    adj:"orange",
+});
+
+addIngredient("yellow_dye",{
+    type:"red_dye",
+    color:"#FFEE00",
+    adj:"yellow",
+});
+
+addIngredient("lime_dye",{
+    type:"red_dye",
+    color:"#00FF00",
+    adj:"lime",
+});
+
+addIngredient("green_dye",{
+    type:"red_dye",
+    color:"#10c100",
+    adj:"green",
+});
+
+addIngredient("cyan_dye",{
+    type:"red_dye",
+    color:"#00FFFF",
+    adj:"cyan",
+});
+
+addIngredient("light_blue_dye",{
+    type:"red_dye",
+    color:"#00CBFF",
+    adj:"light blue",
+});
+
+addIngredient("blue_dye",{
+    type:"red_dye",
+    color:"#0000FF",
+    adj:"blue",
+});
+
+addIngredient("purple_dye",{
+    type:"red_dye",
+    color:"#FF00FF",
+    adj:"purple",
+});
+
+addIngredient("magenta_dye",{
+    type:"red_dye",
+    color:"#BF00FF",
+    adj:"magenta",
+});
+
+addIngredient("pink_dye",{
+    type:"red_dye",
+    color:"#BF00FF",
+    adj:"magenta",
+});
+
+addIngredient("white_dye",{
+    type:"red_dye",
+    color:"#FFFFFF",
+    adj:"white",
+});
+
+addIngredient("light_gray_dye",{
+    type:"red_dye",
+    color:"#C6C6C6",
+    adj:"light_gray",
+});
+
+addIngredient("gray_dye",{
+    type:"red_dye",
+    color:"#F7F7F7",
+    adj:"gray",
+});
+
+addIngredient("black_dye",{
+    type:"red_dye",
+    color:"#000000",
+    adj:"black",
 });
