498f7f2d7e8b662994da7878c7091f9488ed2989
Whatonearth17 <Emadmichael13@gmail.com>
2025-02-08 19:40:23 -0500

    Update Media_pak.js

--- Media_pak.js
+++ Media_pak.js
@@ -1,1 +1,10 @@
-
+addIngredient("dalgona_cookie",{
+    type:"cookie",
+    color:"#E2C576",
+    shape:"fisheye",
+    scale:1.0,
+    keywords:"media,squid,squid_game,dalgona,cookie",
+    dishName:"dalgona_cookies",
+    adj:"dalgona",
+    broken:"breadcrumbs"
+  });
